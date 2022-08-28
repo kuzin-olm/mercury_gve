be2779e0d61ba2aef279fabbeba7130655cd3d9c
kuzin-olm <63362200+kuzin-olm@users.noreply.github.com>
2022-08-28 20:57:01 +0500

    added aioparser with send report is excel file (#6)
    * feat(refactor): разнес все обработчики по смыслу и их регистрацию в предзапуск бота (#3)
    
    * feat(vetdoc): телеграм квиз + парсинг оформленных ветномеров (по фильтру ФИО) по всем предприятиям
    
    * feat(vetdoc): добавлен неблокирующий aioparser на основе базового, плюс шедулер с проверкой завершения работы таски (only once run)
    
    * feat(vetdoc): оставил только aioparser, расширил шедулер, чтобы знать, что задача запущена
    
    * feat(vetdoc): отправка сводной таблицы в телегу эксель файлом

--- merc_gve/services/telegram/task.py
+++ merc_gve/services/telegram/task.py
@@ -1,23 +1,61 @@
 import asyncio
+from datetime import datetime
 from typing import List
 
 import aioschedule
 from merc_gve.settings import logger
 
 
+class CustomJob(aioschedule.Job):
+    def __init__(self, interval, scheduler=None):
+        super().__init__(interval, scheduler)
+        self.is_run = False
+
+    async def run(self):
+        """
+        Run the job and immediately reschedule it.
+
+        :return: The return value returned by the `job_func`
+        """
+        self.is_run = True
+        logger.debug(f"Running task with: {self.is_run}")
+        ret = await self.job_func()
+        self.last_run = datetime.now()
+        self.is_run = False
+        self._schedule_next_run()
+        return ret
+
+
+class CustomScheduler(aioschedule.Scheduler):
+    async def run_pending(self, *args, **kwargs):
+        jobs = [self._run_job(job) for job in self.jobs if job.should_run]
+
+        if not jobs:
+            return [], []
+
+        return await asyncio.wait(jobs, *args, **kwargs)
+
+    def every(self, interval=1):
+        job = CustomJob(interval, self)
+        return job
+
+
 async def add_task_by_minutes(period_minutes: int, func: callable, params: list = None):
     params = params or []
-    aioschedule.every(period_minutes).minutes.do(func, *params)
+    aioschedule.default_scheduler.every(period_minutes).minutes.do(func, *params)
 
 
-def get_user_tasks(user_id: int) -> List[aioschedule.Job]:
+def get_user_tasks(user_id: int) -> List[CustomJob]:
 
-    jobs: List[aioschedule.Job] = aioschedule.jobs
-
-    jobs = [job for job in jobs if job.job_func.args[0]["from"]["id"] == user_id]
+    jobs: List[CustomJob] = aioschedule.default_scheduler.jobs
+    logger.debug(jobs)
+    jobs = [
+        job
+        for job in jobs
+        if job.job_func.args[0]["from"]["id"] == user_id or job.job_func.args[0]["chat"]["id"] == user_id
+    ]
     try:
         logger.debug(jobs[0].job_func.args[0]["from"]["id"])
-        logger.debug(dir(jobs[0]))
     except IndexError:
         pass
     logger.debug(len(jobs))
@@ -27,6 +65,7 @@ def get_user_tasks(user_id: int) -> List[aioschedule.Job]:
 
 async def run_schedule_handler(*args, **kwargs):
     async def run():
+        aioschedule.default_scheduler = CustomScheduler()
         logger.debug("run schedule")
         while True:
             await aioschedule.run_pending()
